bd28d8fbea861c864ce24df3c9bc2754e016fbfc
Timothy M. Shead <tshead@sandia.gov>
2014-09-22 05:11:02 +0000

    Creeping-up on a working edit dialog.

--- web-server/js/slycat.js
+++ web-server/js/slycat.js
@@ -1,6 +1,6 @@
 var slycatApp = angular.module("slycat-application", ["ui.bootstrap"]);
 
-slycatApp.controller("slycat-model-controller", ["$scope", "$http", "$location", function($scope, $http, $location)
+slycatApp.controller("slycat-model-controller", ["$scope", "$http", "$modal", function($scope, $http, $modal)
 {
   $scope.project = {};
   $scope.model = {};
@@ -17,5 +17,36 @@ slycatApp.controller("slycat-model-controller", ["$scope", "$http", "$location",
 
   $scope.edit = function()
   {
+    var edit_model = $modal.open({
+      templateUrl: "slycat-edit-model.html",
+      controller: edit_model_controller,
+      scope: $scope,
+    });
+
+    edit_model.result.then(function(changes)
+    {
+      console.log(changes);
+      $scope.model.name = changes.name;
+      $scope.model.description = changes.description;
+    });
   }
+
+  var edit_model_controller = function ($scope, $modalInstance)
+  {
+    $scope.ok = function()
+    {
+      $modalInstance.close({"name":$scope.model.name, "description":$scope.model.description});
+    }
+
+    $scope.cancel = function()
+    {
+      $modalInstance.dismiss("cancel");
+    }
+
+    $scope.delete = function()
+    {
+      $modalInstance.close();
+    }
+  };
+
 }]);
