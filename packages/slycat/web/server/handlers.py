25ad7375f0683ea7082da3e467583f62748d45d8
Timothy M. Shead <tshead@sandia.gov>
2014-09-29 21:36:48 +0000

    First pass at support for custom model handler plugins.

--- packages/slycat/web/server/handlers.py
+++ packages/slycat/web/server/handlers.py
@@ -323,7 +323,7 @@ def get_model(mid, **kwargs):
 
     if "model-type" in model and model["model-type"] == "parameter-image":
       return slycat.web.server.template.render("model-parameter-image.html", context)
-    
+
     if "model-type" in model and model["model-type"] == "tracer-image":
       return slycat.web.server.template.render("model-tracer-image.html", context)
 
@@ -335,6 +335,17 @@ def get_model(mid, **kwargs):
       context["slycat-plugin-content"] = slycat.web.server.plugin.manager.models[model["model-type"]]["html"](database, model)
 
     return slycat.web.server.template.render("model.html", context)
+
+def get_model_command(mid, command, **kwargs):
+  database = slycat.web.server.database.couchdb.connect()
+  model = database.get("model", mid)
+  project = database.get("project", model["project"])
+  slycat.web.server.authentication.require_project_reader(project)
+
+  if "model-type" in model and model["model-type"] in slycat.web.server.plugin.manager.model_commands.keys():
+    if command in slycat.web.server.plugin.manager.model_commands[model["model-type"]]:
+      return slycat.web.server.plugin.manager.model_commands[model["model-type"]][command]["handler"](database, model, command, **kwargs)
+  raise cherrypy.HTTPError("400 Unknown command: %s" % command)
 
 @cherrypy.tools.json_in(on = True)
 def put_model(mid):
